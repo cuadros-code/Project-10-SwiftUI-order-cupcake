f74dd95c4e3bda3b2901631280dafec59fe84482
kevin cuadros <kevindavidcuadros@gmail.com>
2024-12-23 12:19:38 -0500

    Create README

--- Project-10-SwiftUI-order-cupcake/ContentView.swift
+++ Project-10-SwiftUI-order-cupcake/ContentView.swift
@@ -7,16 +7,38 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: String
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
     }
+    
+    func loadData() async {
+        
+    }
+    
 }
 
 #Preview {
---
+++ README.md
@@ -0,0 +1,5 @@
+#  Project 10
+
+- async / await
+- .task
+
