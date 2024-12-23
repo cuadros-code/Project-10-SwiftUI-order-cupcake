0bc4e33ad26454cb675f9322949faf22143b9b30
kevin cuadros <kevindavidcuadros@gmail.com>
2024-12-23 11:58:29 -0500

    Initial Commit

---
+++ Project-10-SwiftUI-order-cupcake/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-10-SwiftUI-order-cupcake
+//
+//  Created by Kevin Cuadros on 23/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-10-SwiftUI-order-cupcake/Project_10_SwiftUI_order_cupcakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_10_SwiftUI_order_cupcakeApp.swift
+//  Project-10-SwiftUI-order-cupcake
+//
+//  Created by Kevin Cuadros on 23/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_10_SwiftUI_order_cupcakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
