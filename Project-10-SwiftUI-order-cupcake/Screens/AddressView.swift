b93db209dac9439acfaed1117f54f7b2dab524ef
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-03 17:00:27 -0500

    create order class

---
+++ Project-10-SwiftUI-order-cupcake/Screens/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  Project-10-SwiftUI-order-cupcake
+//
+//  Created by Kevin Cuadros on 3/01/25.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
