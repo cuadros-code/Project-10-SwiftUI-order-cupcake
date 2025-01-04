82925ce54513e631155d1b52f433e0e30121f9bc
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-04 12:18:33 -0500

    Preparing for checkout

--- Project-10-SwiftUI-order-cupcake/Screens/CheckoutView.swift
+++ Project-10-SwiftUI-order-cupcake/Screens/CheckoutView.swift
@@ -11,10 +11,30 @@ struct CheckoutView: View {
     var order: Order
     
     var body: some View {
-        Form {
-            Section {
+        ScrollView {
+            VStack {
+                AsyncImage(
+                    url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"),
+                    scale: 3
+                ) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                
+                Button("Place Order", action: {})
+                    .padding()
             }
         }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
